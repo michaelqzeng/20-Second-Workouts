cf18c5db4244189ec60fac957ac48a0629e29a27
Michael Zeng <qzeng96@gmail.com>
2018-09-10 17:10:29 -0700

    Initial Commit

---
+++ ProjectWorkout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectWorkout
+//
+//  Created by Michael Zeng on 9/10/18.
+//  Copyright © 2018 Michael Zeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
