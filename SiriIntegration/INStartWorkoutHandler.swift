377bbabc234a1729280e98de2f3d95ab9c4b1809
Michael Zeng <michaelzeng@michaels-macbook-pro.local>
2018-11-16 08:31:14 -0800

    - Siri Integration

---
+++ SiriIntegration/INStartWorkoutHandler.swift
@@ -0,0 +1,14 @@
+//
+//  INStartWorkoutHandler.swift
+//  SiriIntegration
+//
+//  Created by Michael Zeng on 11/15/18.
+//  Copyright © 2018 Michael Zeng. All rights reserved.
+//
+
+import Intents
+
+// Opens up a workout
+class INStartWorkoutHandler: NSObject, INStartWorkoutIntentHandling {
+    
+}
---
+++ SiriIntegration/IntentHandler.swift
@@ -0,0 +1,13 @@
+//
+//  IntentHandler.swift
+//  SiriIntegration
+//
+//  Created by Michael Zeng on 11/14/18.
+//  Copyright © 2018 Michael Zeng. All rights reserved.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+}
